2249b0b57605852f767daefbc03c9681912d8c3a
commakozzi <commakozzi1@gmail.com>
2016-04-11 16:58:01 -0400

    Update to doCalc.java

--- Calculator/doCalc.java
+++ Calculator/doCalc.java
@@ -1,33 +1,34 @@
 import java.util.Scanner;
 
-class doCalc(int ... a) {
+class doCalc {
 
-  double input1, input2 = 0;
-  String operator;
-  Scanner userInput = new Scanner(System.in);
-  double additionResult = input1 + input2;
-  double subtractionResult = input1 - input2;
+  public static void main(String args[]) {
 
-  System.out.println("Enter first number:");
-  input1 = userInput.nextDouble();
+    double input1, input2 = 0;
+    String operator;
+    Scanner userInput = new Scanner(System.in);
+    double additionResult = input1 + input2;
+    double subtractionResult = input1 - input2;
 
-  System.out.println("Enter second number:");
-  input2 = userInput.nextDouble();
+    System.out.println("Enter first number:");
+    input1 = userInput.nextDouble();
 
-  System.out.println("Enter your operator:");
-  operator = userInput.next();
+    System.out.println("Enter second number:");
+    input2 = userInput.nextDouble();
 
-  switch(operator) {
+    System.out.println("Enter your operator:");
+    operator = userInput.next();
 
-    case "+":
-      System.out.println(additionResult);
-      break;
+    switch(operator) {
 
-    case "-":
-      System.out.println(subtractionResult);
-      break;
+      case "+":
+        System.out.println(additionResult);
+        break;
 
+      case "-":
+        System.out.println(subtractionResult);
+        break;
+
+    }
   }
-
-
 }
